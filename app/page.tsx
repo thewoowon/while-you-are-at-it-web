20d480357ec20a0f9ec079bce23ebd1383ed8451
thewoowon <thewoowon@naver.com>
2025-01-23 12:35:00 +0900

    feat: global work

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import {
+  Section1,
+  Section2,
+  Section3,
+  Section4,
+  Section5,
+} from "@/components/layout/Sections";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    <div className="items-center justify-items-center min-h-screen w-full">
+      <main className="flex flex-col w-full">
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+      </main>
+      <footer className="flex flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
