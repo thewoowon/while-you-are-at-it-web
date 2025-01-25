33b9e1408f4c7a71f51cdf66abdbb0ab9ac21443
thewoowon <thewoowon@naver.com>
2025-01-25 14:00:33 +0900

    feat: main page section smooth style

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,28 @@ import {
   Section4,
   Section5,
 } from "@/components/layout/Sections";
+import Section6 from "@/components/layout/Sections/Section6";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen w-full">
-      <main className="flex flex-col w-full">
+    <main className="items-center justify-items-center min-h-screen w-full">
+      <div
+        className="flex flex-col w-full relative"
+        style={{
+          overflow: "hidden",
+          scrollBehavior: "smooth",
+          // 스크롤바 숨김
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+        }}
+      >
         <Section1 />
         <Section2 />
         <Section3 />
         <Section4 />
         <Section5 />
-      </main>
-      <footer className="flex flex-wrap items-center justify-center"></footer>
-    </div>
+        <Section6 />
+      </div>
+    </main>
   );
 }
