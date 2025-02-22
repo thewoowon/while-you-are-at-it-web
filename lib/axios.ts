99b05d87ed91b807e7ae23793dd4fcc6e10fe080
thewoowon <thewoowon@naver.com>
2025-02-22 11:22:13 +0900

    refactor: axios edited

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
-import qs from "qs";
+import { API_PREFIX } from "@/constants";
+
+const basicUrl = `${process.env.NEXT_PUBLIC_API_URL}${API_PREFIX}`;
 
 // Axios 인스턴스 생성
 const customAxios = axios.create({
-  withCredentials: true, // CORS 요청 시 인증 정보를 전송하도록 설정
-  // 기타 필요한 기본 설정 추가
-  baseURL: "https://api.lululala.at",
-  paramsSerializer: (params) => {
-    return qs.stringify(params, { arrayFormat: "repeat" });
+  baseURL: basicUrl,
+  timeout: 10000, // 10초 제한
+  headers: {
+    "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 export default customAxios;
