0a21aa4f404c47b3fc4d7ca061ec2850151c6753
thewoowon <thewoowon@naver.com>
2025-02-22 11:18:04 +0900

    feat: 이용약관, 개인정보처리방침 페이지

---
+++ app/docs/page.tsx
@@ -0,0 +1,9 @@
+const DocsPage = () => {
+  return (
+    <main className="max-w-2xl mx-auto pt-[64px] min-h-screen">
+      <section className="pt-[64px] pb-[52px] prose prose-lg"></section>
+    </main>
+  );
+};
+
+export default DocsPage;
